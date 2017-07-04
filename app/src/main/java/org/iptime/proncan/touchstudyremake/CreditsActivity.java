e6f46afe7d04e0d868d16397e76583bb839f4392
PronCan <twojini98@gmail.com>
2017-07-04 11:51:45 +0900

    commit 2
    - error : change activity - Credits

--- app/src/main/java/org/iptime/proncan/touchstudyremake/CreditsActivity.java
+++ app/src/main/java/org/iptime/proncan/touchstudyremake/CreditsActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by 호창 on 2017-07-04.
  */
 
 public class CreditsActivity extends Activity implements View.OnClickListener {
+    Button btn_gotoMain = (Button) findViewById(R.id.btn_gotoMain);
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,10 @@ public class CreditsActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-
+        switch(v.getId()) {
+            case R.id.btn_gotoMain :
+                finish();
+                break;
+        }
     }
 }
